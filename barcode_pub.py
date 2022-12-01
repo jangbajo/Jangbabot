c224e31bf945530cf35d3315513f64cc2c90be09
신효진 <76603536+hyojin-shin@users.noreply.github.com>
2022-12-01 22:55:25 +0900

    Add files via upload
    add barcode_pub.py map_navigation.cpp

--- barcode_pub.py
+++ barcode_pub.py
@@ -5,28 +5,36 @@ import pyzbar.pyzbar as pyzbar
 from std_msgs.msg import Int32MultiArray,Int32
 
 rospy.init_node('detection_pub')
-pub = rospy.Publisher('detection',Int32,queue_size=10)
+#섭이랑은 항상 스핀 원스가 있어야한다
 
-barcode_number = [1,2,3,4]
+pub = rospy.Publisher('detection_success',Int32,queue_size=10)
+ok = Int32()
+ok = 1;
 num=[0,0,0,0]
 
-cap = cv2.VideoCapture(0)
-while True:
-	success, frame = cap.read()
-	if success:
-		for code in pyzbar.decode(frame):
-			my_code = Int32()
-			my_code = int(code.data.decode('utf-8'))
-			for i in range(4):
-				if my_code == barcode_number[i] and num[i]==0 :
-					print("인식 성공 : ",my_code)
-					pub.publish(my_code)
-					num[i]=1
-#		cv2.imshow("cam",frame)
-        
-		key = cv2.waitKey(1)
-		if key ==27:
-			break
-                
-cap.release()
-cv2.destroyALLWindows()
+
+
+def callback(msg):
+    print('msg.data는 {}입니다'.format(msg.data))
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if success:
+            for code in pyzbar.decode(frame):
+                my_code = Int32()
+                my_code = int(code.data.decode('utf-8'))
+                print('{}을 인식했습니다'.format(my_code))
+                for i in range(4):
+                    if msg.data == my_code and num[i]==0 :
+                        print('인식 성공 : ',my_code)
+                        pub.publish(ok)
+                        num[i]=1
+
+    cap.release()
+    cv2.destroyALLWindows()
+
+
+sub = rospy.Subscriber("detection_start",Int32,callback)
+
+rospy.spin()
+
---
+++ map_navigation.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+#include <unistd.h>
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include "std_msgs/String.h"
+#include "std_msgs/Int32.h"
+#include <string>
+
+using namespace std;
+
+#define UIMODE		1
+#define AUTOMODE	2
+
+int mode = UIMODE;
+char order[100];
+
+/** function declarations **/
+bool moveToGoal(double xGoal, double yGoal, double zGoal, double wGoal);
+void startAuto(int num, char* orderlist);
+
+/** declare the coordinates of interest **/
+
+//--------------------------------//
+int section_number = 0;
+int breaktime = 0;
+//ros::Rate rate(5);
+//ros::init(argc, argv, "map_navigation_node");
+//ros::NodeHandle n;
+//------------------------------//
+
+
+ros::Publisher mode_pub;
+ros::Publisher detection_pub;
+struct Pos{
+
+	double x;
+	double y;
+	double z;
+	double w;
+};
+
+struct Pos pos0 = {1.88, -1.73, -0.5, 0.8};
+//struct Pos pos1 = {2.83, -3.23, -0.50, 0.86};
+struct Pos pos1 = {3.01, -3.16, -0.5, 0.8};
+struct Pos pos2 = {3.87, -2.67, 0.1, 0.9};
+struct Pos pos3 = {3.09, -1.95, -0.9, 0.2};
+struct Pos pos4 = {3.28, -1.27, 0.2, 0.9};
+
+bool goalReached = false;
+/*
+void modeCallback(const std_msgs::Int32::ConstPtr& msg){
+	ROS_INFO("mode: [%d]", msg->data);
+	mode = msg->data;
+	cout<< mode << '\n';
+}
+*/
+void autoCallback(const std_msgs::String::ConstPtr& msg){
+        ROS_INFO("list order: [%s]", msg->data.c_str());
+	mode = AUTOMODE;
+
+	int ordernum = strlen(msg->data.c_str());
+	cout << "ordernum: " << ordernum << '\n';
+	strcpy(order, msg->data.c_str());
+	cout << order << '\n';
+	startAuto(ordernum, order);
+
+
+}
+//-----------------------//
+void detection_success_callback(const std_msgs::Int32::ConstPtr& msg){
+	ros::Rate rate(5);
+	while(breaktime<=25){
+		breaktime++;
+		rate.sleep();
+	}
+
+}
+
+
+
+//int main(int argc, char** argv){
+//	ros::init(argc, argv, "map_navigation_node");
+//	ros::NodeHandle n;
+//	ros::Rate rate(5);
+
+//        mode_pub = n.advertise<std_msgs::Int32>("uimode", 1000);
+
+//	ros::Subscriber autosub = n.subscribe("listorder", 1000, autoCallback);
+//	ros::spin();
+//}
+
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "map_navigation_node");
+	ros::NodeHandle n;
+//	ros::Rate rate(5);
+
+        mode_pub = n.advertise<std_msgs::Int32>("uimode", 1000);
+
+	detection_pub = n.advertise<std_msgs::Int32>("detection_start",1000);
+	ros::Subscriber autosub = n.subscribe("listorder", 1000, autoCallback);
+	ros::Subscriber detection_sub = n.subscribe("detection_success",1000,detection_success_callback);
+	ros::spin();
+
+}
+
+
+
+
+
+//------------------------//
+void startAuto(int num, char* orderlist){
+//---------//
+	std_msgs::Int32 message;
+	message.data = section_number;
+//---------//
+	for(int i = 0; i < num; i++){
+		char section = orderlist[i];
+
+		cout << section << "구역 이동 시작" << '\n';
+		if(section == '1'){
+			section_number +=1;
+			goalReached = moveToGoal(pos1.x, pos1.y, pos1.z, pos1.w);
+			//도착을 했으면 도착했으니까 바코드 감지 시작해줘 라는걸 펍해야한다//
+			detection_pub.publish(message);
+			//디택션이 성공하면 성공했다는 사실을 섭을 통해 얻고, 몇초 쉰다음에 다음걸 진행한다//
+			ros::spinOnce();
+                }
+			
+		else if (section == '2'){
+			section_number +=1;
+			goalReached = moveToGoal(pos2.x, pos2.y, pos2.z, pos2.w);
+			//-------------------------------//
+			detection_pub.publish(message);
+			ros::spinOnce();
+			//----------------------------------//
+			
+		}
+		else if (section == '3'){
+			section_number +=1;
+			goalReached = moveToGoal(pos3.x, pos3.y, pos3.z, pos3.w);
+			detection_pub.publish(message);
+			ros::spinOnce();
+		}
+		else if (section == '4'){
+			section_number +=1;
+			goalReached = moveToGoal(pos4.x, pos4.y, pos4.z, pos4.w);
+			detection_pub.publish(message);
+			ros::spinOnce();
+		}
+
+		if (goalReached){
+			ROS_INFO(":) success!!!");
+		}
+		else{
+			ROS_INFO(":( fail ..");
+		}
+	}
+
+	ROS_INFO("zero section go!");
+	goalReached = moveToGoal(pos1.x, pos1.y, 0.0, 1.0);
+	goalReached = moveToGoal(pos0.x, pos0.y, pos0.z, pos0.w);
+	if (goalReached){
+		ROS_INFO("success!!!");
+	}
+	else{
+		ROS_INFO("fail ..");
+	}
+
+	mode = UIMODE;
+//	ros::Publisher mode_pub = n.advertise<std_msgs::Int32>("uimode", 1000);
+
+	std_msgs::Int32 msg;
+	msg.data = UIMODE;
+	mode_pub.publish(msg);
+	ROS_INFO("mode_publish send !");
+
+}
+
+
+//int main(int argc, char** argv){
+//	ros::init(argc, argv, "map_navigation_node");
+//	ros::NodeHandle n;
+//	ros::Rate rate(5);
+//
+//        mode_pub = n.advertise<std_msgs::Int32>("uimode", 1000);
+
+//	ros::Publisher detection_pub = n.advertise<std_msgs::Int32>("detection_start",1000);
+//	ros::Subscriber autosub = n.subscribe("listorder", 1000, autoCallback);
+//	ros::Subscriber detection_sub = n.subscribe("detection_success",1000,detection_success_callback);
+//	ros::spin();
+
+//}
+
+
+bool moveToGoal(double xGoal, double yGoal, double zGoal, double wGoal){
+
+	//define a client for to send goal requests to the move_base server through a SimpleActionClient
+	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
+
+	//wait for the action server to come up
+	while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
+
+	move_base_msgs::MoveBaseGoal goal;
+
+	//set up the frame parameters
+	goal.target_pose.header.frame_id = "map";
+	goal.target_pose.header.stamp = ros::Time::now();
+
+	/* moving towards the goal*/
+
+	goal.target_pose.pose.position.x =  xGoal;
+	goal.target_pose.pose.position.y =  yGoal;
+	goal.target_pose.pose.position.z =  0.0;
+	goal.target_pose.pose.orientation.x = 0.0;
+	goal.target_pose.pose.orientation.y = 0.0;
+	goal.target_pose.pose.orientation.z = zGoal;
+	goal.target_pose.pose.orientation.w = wGoal;
+
+	ROS_INFO("Sending goal location ...");
+	ac.sendGoal(goal);
+
+	ac.waitForResult();
+
+	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+		ROS_INFO("You have reached the destination");
+		return true;
+	}
+	else{
+		ROS_INFO("The robot failed to reach the destination");
+		return false;
+	}
+
+}
+
